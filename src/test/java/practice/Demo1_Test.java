4ede4e7a8694b95b96b05467e0eb2b6c72cd2eb7
ramya <ramya@192.168.1.35>
2026-01-09 15:04:09 +0530

    Comitted

---
+++ src/test/java/practice/Demo1_Test.java
@@ -0,0 +1,15 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Demo1_Test {
+	
+	@Test(groups="Smoke")
+	
+	public void SchoolTest()
+	{
+		System.out.println("School");
+	}
+
+
+}
---
+++ src/test/java/practice/Demo2_Test.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Demo2_Test {
+	@Test(groups="Regression")
+	public void HPTest()
+	{
+		System.out.println("Harry");
+	}
+
+}
